b3952a66646558143f58e470688c629ade41172b
Andy Hoffner <ahoffner@gmail.com>
2016-11-01 16:50:25 -0600

    Fixing syntax error after manual code merge

--- bootbox.js
+++ bootbox.js
@@ -663,6 +663,7 @@
 
     if (options.size === "extra-large") {
       innerDialog.addClass("modal-xl");
+    }
     else if (options.size === "large") {
       innerDialog.addClass("modal-lg");
     } else if (options.size === "small") {
